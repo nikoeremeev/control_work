081d2cbc9e3abb2cc7b6454b43f3f24196ccd4cd
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-04-02 10:40:20 +0300

    решение задачи

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array= {"hello", "2", "world", "two", "computer", ":)"};
+string[] new_array = new string [array.Length];
+int p = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        new_array[p] = array[i];
+        p++;
+    }
+}
+for (int i = 0; i <= p; i++)
+{
+    Console.Write(new_array[i] + " ");
+}
